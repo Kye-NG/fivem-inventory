b3fc1f43495397c09473b4acb5432c9d44804402
Kye-NG <kyenormangill98@gmail.com>
2021-03-17 20:22:55 +1100

    [INV] Add VUE and Axios to frontend. Convert jquery logic to vue library.

--- html/js/index.js
+++ html/js/index.js
@@ -1,36 +1,50 @@
-$(function() {
-  function display(bool) {
-    if (bool) {
-      $("#test").show();
-    } else {
-      $("#test").hide();
-    }
-  }
-
-  display(false);
-
-  $.post('https://fivem-inventory/main', JSON.stringify({ text: 'Loaded'}));
-
-
-  document.onkeyup = function (data) {
-    if (data.which == 9) {
-      $.post('https://fivem-inventory/exit', JSON.stringify({}));
-    }
-  };
-
-  // $.post('http://fivem-inventory/main', JSON.stringify({ text: 'hello'}));
-
-  window.addEventListener('message', function(event) {
-    const item = event.data;
-
-    if (item.type === "ui") {
-      if (item.status == true) {
-        display(true)
-      } else {
-        display(false)
-      }
-    }
-  })
-})
 
 console.log('test');
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    menuOpen: false
+  },
+  created: function() {
+    window.addEventListener('message', this.setupMessageListener);
+    window.addEventListener('keyup', this.setupCloseListener);
+
+    axios.post('https://fivem-inventory/main', { text: 'loaded with vue' }).then(data => {
+      console.log(data);
+    }).catch(e => {
+      console.log(e);
+    })
+  },
+  destroyed: function() {
+    window.removeEventListener('message', this.setupMessageListener);
+    window.removeEventListener('keyup', this.setupCloseListener);
+  },
+  methods: {
+    setupMessageListener(event) {
+      const item = event.data;
+
+      if (item.type === "ui") {
+        if (item.status == true) {
+          this.display(true)
+        } else {
+          this.display(false)
+        }
+      }
+    },
+    setupCloseListener(data) {
+      if (data.which == 9) {
+        axios.post('https://fivem-inventory/exit', {}).then(data => {
+          console.log(data);
+        }).catch(e => {
+          console.log(e);
+        })
+      }
+    },
+    display(bool) {
+      this.menuOpen = bool;
+
+      console.log(`Set menu bool to: ${bool}`);
+    }
+  }
+})
