30b67cb625eece07f983a07eb2cb5ef3d92bc8ca
Kye-NG <kyenormangill98@gmail.com>
2021-03-22 08:49:27 +1100

    [INV] Update inventory sql to be more convenient. Added foreign keys for items.

--- create_table.sql
+++ create_table.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE fivem;
+
 CREATE TABLE items (
   item_id int NOT NULL AUTO_INCREMENT,
   display_name VARCHAR(100) NOT NULL DEFAULT 'Unknown',
@@ -5,11 +7,12 @@ CREATE TABLE items (
   PRIMARY KEY (item_id)
 );
 
-CREATE TABLE inventories (
-  id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE user_inventory (
   license VARCHAR(100),
-  items VARCHAR(1000),
-  PRIMARY KEY (id)
+  item_id int NOT NULL,
+  quantity int NOT NULL DEFAULT 1,
+  KEY item_id (item_id),
+  CONSTRAINT fk_item_id FOREIGN KEY (item_id) REFERENCES items (item_id)
 );
 
 INSERT INTO items (display_name, type)
@@ -23,3 +26,29 @@ INSERT INTO items (display_name, type)
 
 INSERT INTO items (display_name, type)
   VALUES ('Machete', 2);
+
+INSERT INTO items (display_name, type)
+  VALUES ('Test item 1', 2);
+
+INSERT INTO items (display_name, type)
+  VALUES ('Test item 2', 2);
+
+INSERT INTO items (display_name, type)
+  VALUES ('Test item 3', 2);
+
+INSERT INTO items (display_name, type)
+  VALUES ('Test item 4', 2);
+
+INSERT INTO items (display_name, type)
+  VALUES ('Test item 5', 2);
+
+-- test item giving
+INSERT INTO user_inventory(license, item_id, quantity) VALUES ('license:8c60b253d34fd38cada69c4b1daf93debf01030a', 1, 50);
+INSERT INTO user_inventory(license, item_id, quantity) VALUES ('license:8c60b253d34fd38cada69c4b1daf93debf01030a', 2, 1);
+INSERT INTO user_inventory(license, item_id, quantity) VALUES ('license:8c60b253d34fd38cada69c4b1daf93debf01030a', 3, 1);
+INSERT INTO user_inventory(license, item_id, quantity) VALUES ('license:8c60b253d34fd38cada69c4b1daf93debf01030a', 4, 1);
+INSERT INTO user_inventory(license, item_id, quantity) VALUES ('license:8c60b253d34fd38cada69c4b1daf93debf01030a', 5, 1);
+INSERT INTO user_inventory(license, item_id, quantity) VALUES ('license:8c60b253d34fd38cada69c4b1daf93debf01030a', 6, 1);
+INSERT INTO user_inventory(license, item_id, quantity) VALUES ('license:8c60b253d34fd38cada69c4b1daf93debf01030a', 7, 1);
+INSERT INTO user_inventory(license, item_id, quantity) VALUES ('license:8c60b253d34fd38cada69c4b1daf93debf01030a', 8, 1);
+INSERT INTO user_inventory(license, item_id, quantity) VALUES ('license:8c60b253d34fd38cada69c4b1daf93debf01030a', 9, 1);
